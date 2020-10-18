c4b5a0fece10fdf337c09437fdaa92f1db5e2765
mbagnsk <56918406+mbagnsk@users.noreply.github.com>
2020-10-18 19:08:02 +0200

    Add files via upload

---
+++ LAB1/sources/lab_a1.m
@@ -0,0 +1,21 @@
+clear all; close all;
+a=3;
+b=1;
+p=[1 a b];
+delta=a*a-4*b;
+s_roots=roots(p);
+out=sim('labsim');
+t=out.tout;
+simout=out.simout;
+figure, hold on, grid on;
+plot(t,simout);
+xlabel('t');
+ylabel('\lambda (t)');
+
+figure, hold on, grid on;
+plot(real(s_roots), imag(s_roots), 'x');
+yline(0,':');
+xline(0,':');
+axis([-5 5 -5 5]);
+xlabel('Re(z)');
+ylabel('Im(z)');
